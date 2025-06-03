9acd93a470c8eb5284f51618e2c0b62ba0b86f9e
Marek Pribyl <mr.pribyl@gmail.com>
2025-06-03 14:58:54 +0200

    order_items table dropped, order table update to align with domain changes

--- src/main/resources/db/migration/V2__add_sku_to_products.sql
+++ src/main/resources/db/migration/V2__add_sku_to_products.sql
@@ -3,5 +3,3 @@ ALTER TABLE products ADD sku VARCHAR(50) NOT NULL ;
 UPDATE products SET sku = CONCAT('LEGACY-', CAST(id AS VARCHAR)) WHERE sku IS NULL;
 
 ALTER TABLE products ADD CONSTRAINT uk_products_sku UNIQUE (sku);
-
-CREATE INDEX idx_products_sku ON products(sku);
